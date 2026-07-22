89c111aa109a555f7f7cb0a9daecf6633bd51e19
Marcus Krejpowicz <marcus.krejpowicz@storyblok.com>
2026-07-22 13:50:05 +0200

    Hide editor pane, terminal fills full height

--- src/components/main/MainArea.tsx
+++ src/components/main/MainArea.tsx
@@ -2,11 +2,9 @@ import { useEffect } from "react";
 import {
   Group,
   Panel,
-  Separator,
   usePanelRef,
 } from "react-resizable-panels";
 import TerminalZone from "./TerminalZone";
-import EditorZone from "./EditorZone";
 import { useTerminalStore } from "../../stores/terminalStore";
 
 export default function MainArea() {
@@ -38,7 +36,7 @@ export default function MainArea() {
     <Group orientation="vertical" className="flex h-full w-full">
       <Panel
         panelRef={terminalPanelRef}
-        defaultSize="40%"
+        defaultSize="100%"
         minSize="10%"
         collapsedSize={0}
         collapsible
@@ -52,10 +50,12 @@ export default function MainArea() {
           onToggleCollapse={handleToggleCollapse}
         />
       </Panel>
+      {/* Editor hidden for now
       <Separator className="h-px bg-[var(--color-surface0)] transition-colors hover:bg-[var(--color-blue)]" />
       <Panel defaultSize="60%" minSize="10%" className="bg-[var(--color-base)]">
         <EditorZone />
       </Panel>
+      */}
     </Group>
   );
 }
